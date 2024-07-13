eb31316b32276692ae43cab6cfa14097d72e53e8
Rasyid Amrul Fathony <rasyidamrulfathony@gmail.com>
2024-07-13 23:53:09 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// This universal for all landing page website
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
